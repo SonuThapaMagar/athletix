cc299e2ac95ab82168814e978c62e90d3fd16a32
sonu <sonumagar787@gmail.com>
2025-10-30 21:07:27 +0545

    backend setup

---
+++ src/test/java/com/athletix/AthletixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athletix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
